16d5ffce2992931c379eca3a48cae7f86f3acc01
slash0t <minenion2@gmail.com>
2021-09-05 11:33:27 +0300

    Reworked news loop to work on generator

--- utils/news_parser/news_publisher.py
+++ utils/news_parser/news_publisher.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import time
 from datetime import datetime, timedelta
 
 from data.config import CHANNEL_ID
@@ -12,40 +13,42 @@ from utils.news_parser.parser import parse_hltv
 DATE_FORMAT = '%a, %d %b %Y %H:%M GMT'
 
 
-async def publish_news(sleep_time):
-    last_post = get_last_article()
-    if last_post:
-        last_post_date = last_post.date
-    else:
-        last_post_date = datetime.utcnow() - timedelta(days=7)
-
-    articles_data = parse_hltv(last_post_date)
-
-    for article_data in articles_data[::-1]:
-        article: Article = save_article(**article_data)
-
-        if article.event:
-            info_text = f'<em>{article.place} - {article.event}</em>'
+def data_generator(sleep_time):
+    while True:
+        last_post = get_last_article()
+        if last_post:
+            last_post_date = last_post.date
         else:
-            info_text = f'<em>{article.place}</em>'
-        fragment_text = f'{article.fragment}\n' if article.fragment else ''
-        date_text = article.date.strftime(DATE_FORMAT)
+            last_post_date = datetime.utcnow() - timedelta(days=7)
+        yield parse_hltv(last_post_date)
+        time.sleep(sleep_time)
 
-        news_message_text = f'<b>{article.title}</b>\n' \
-                            f'{info_text}\n\n' \
-                            f'{fragment_text}' \
-                            f'<em>by {article.author}, at {date_text}</em>'
 
-        await bot.send_message(CHANNEL_ID,
-                               news_message_text,
-                               reply_markup=await get_url_keyboard(article.url)
-                               )
-        logging.info(f'Saved article: {article.title}')
-        await asyncio.sleep(3)
+async def publish_news(sleep_time):
+    for articles_data in data_generator(sleep_time):
+        for article_data in articles_data[::-1]:
+            article: Article = save_article(**article_data)
 
-    logging.info(f'{sleep_time} seconds until next news update')
-    await asyncio.sleep(sleep_time)
-    await publish_news(sleep_time)
+            if article.event:
+                info_text = f'<em>{article.place} - {article.event}</em>'
+            else:
+                info_text = f'<em>{article.place}</em>'
+            fragment_text = f'{article.fragment}\n' if article.fragment else ''
+            date_text = article.date.strftime(DATE_FORMAT)
+
+            news_message_text = f'<b>{article.title}</b>\n' \
+                                f'{info_text}\n\n' \
+                                f'{fragment_text}' \
+                                f'<em>by {article.author}, at {date_text}</em>'
+
+            await bot.send_message(CHANNEL_ID,
+                                   news_message_text,
+                                   reply_markup=await get_url_keyboard(article.url)
+                                   )
+            logging.info(f'Saved article: {article.title}')
+            await asyncio.sleep(3)
+
+        logging.info(f'{sleep_time} seconds until next news update')
 
 
 async def start_news_publishing(sleep_time):
